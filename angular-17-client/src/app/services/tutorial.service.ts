8f668aa6ee0acffc31363e656ce75ebf275ce967
Palla Sai Shashank <pallasai_shashank@epam.com>
2025-09-30 17:39:46 +0530

    changed base api

--- angular-17-client/src/app/services/tutorial.service.ts
+++ angular-17-client/src/app/services/tutorial.service.ts
@@ -5,7 +5,7 @@ import { Tutorial } from '../models/tutorial.model';
 
 // const baseUrl = 'http://localhost:8080/api/tutorials';
 
-const baseUrl = 'http://springboot-service.dexter-1.svc.cluster.local:32023/api/tutorials';
+const baseUrl = '/api/tutorials';
 
 @Injectable({
   providedIn: 'root',
