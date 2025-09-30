28fa13556b1e3fed8433bd4c0da274ce6057ced1
Palla Sai Shashank <pallasai_shashank@epam.com>
2025-09-30 11:49:39 +0530

    added files

--- terraform/modules/storage_container/main.tf
+++ terraform/modules/storage_container/main.tf
@@ -3,5 +3,5 @@ resource "azurerm_storage_container" "DevOps_storage_container" {
 
     storage_account_name  = var.storage_account[each.value.storage_account]
     name                  = each.value.name
-    container_access_type = each.value
+    container_access_type = each.value.container_access_type
 }
