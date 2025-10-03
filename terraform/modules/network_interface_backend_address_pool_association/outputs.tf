d36ebcc71d47c69487725434a79972fb3298ef81
Palla Sai Shashank <pallasai_shashank@epam.com>
2025-10-03 12:43:48 +0530

    files

--- terraform/modules/network_interface_backend_address_pool_association/outputs.tf
+++ terraform/modules/network_interface_backend_address_pool_association/outputs.tf
@@ -1,4 +1,4 @@
 output "nic_backend_pool_association_id" {
     description = "Map of NIC backend pool IDs keyed by NIC name"
-    value = { for nic in azurerm_network_interface_backend_address_pool_association.DevOps_nic_backend_address_pool_association : nic.network_interface => nic.id }
+    value = { for nic in azurerm_network_interface_backend_address_pool_association.DevOps_nic_backend_address_pool_association : nic.network_interface_id => nic.id }
 }
